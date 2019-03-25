48810e1a728d46670d422f6458ced539d247a235
vlu00 <vlu00@stuy.edu>
2019-03-25 14:51:42 -0400

    Beginning on eval

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator{
+
+  public static boolean isNum(String s){
+    
+  }
+
+  public static double eval(String s){
+    MyDeque A = new MyDeque(s.length());
+    //int size = s.length;
+    //int i = 0; //s.length();
+    while (s.length > 0) {
+      double num = Integer.parstInt(s.split(" "));
+      if
+      s = s.substring(2);
+      i++;
+    }
+    double num =
+  }
+}
